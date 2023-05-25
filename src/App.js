d42e5c224ae0061e7af6a853bc03eb66b733dbad
Janinenw <janine.wexler@gmail.com>
2023-05-24 22:38:02 -0400

    connected to backend locally

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ 
+  const URL = "http://localhost:4000/";
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      console.log(data);
+      
+    } catch (error) {
+      console.error("Error fetching data- stop trying to make fetch happen:", error);
+      
+    }
+  };
+
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
