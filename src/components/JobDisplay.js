826d2e8a0d5a417cafd9f964ed589ba4719fa8b7
Janinenw <janine.wexler@gmail.com>
2023-06-03 18:36:46 -0400

    delete working correctly

--- src/components/JobDisplay.js
+++ src/components/JobDisplay.js
@@ -1,9 +1,53 @@
+// import React from 'react';
+// import { useJobsContext } from '../hooks/useJobsContext';
+// import { useAuthContext } from '../hooks/useAuthContext';
+
+// const JobDisplay = ({ job }) => {
+//   const { dispatch } = useJobsContext();
+//   const { user } = useAuthContext();
+
+//   const handleClick = async () => {
+//     if (!user) {
+//       return;
+//     }
+
+//     const response = await fetch(`http://localhost:4000/jobs/delete/${job._id}`, {
+//       method: 'DELETE',
+//       headers: {
+//         'Authorization': `Bearer ${user.token}`
+//       }
+//     });
+//     const json = await response.json();
+
+//     if (response.ok) {
+//       dispatch({ type: 'DELETE_JOB', payload: json });
+//     }
+//   };
+
+//   return (
+//     <div className="job-details">
+//       <h4>{job.company}</h4>
+//       <p><strong>Position: </strong>{job.position}</p>
+//       <p><strong>Application Status: </strong>{job.appStatus}</p>
+//       <p><strong>Next Steps: </strong>{job.nextSteps}</p>
+//       <p><strong>Deadline: </strong>{job.deadline}</p>
+//       <p><strong>Date Applied: </strong>{job.dateApplied}</p>
+//       <p><strong>Important Date: </strong>{job.importantDate}</p>
+//       <p><strong>Notes: </strong>{job.notes}</p>
+//       <p><strong>Final Result: </strong>{job.finalResult}</p>
+  
+//       <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
+//     </div>
+//   );
+// };
+
+// export default JobDisplay; 
+
+
 import React from 'react';
-import { useJobsContext } from '../hooks/useJobsContext';
 import { useAuthContext } from '../hooks/useAuthContext';
 
-const JobDisplay = ({ job }) => {
-  const { dispatch } = useJobsContext();
+const JobDisplay = ({ job, onDeleteJob }) => {
   const { user } = useAuthContext();
 
   const handleClick = async () => {
@@ -17,28 +61,28 @@ const JobDisplay = ({ job }) => {
         'Authorization': `Bearer ${user.token}`
       }
     });
-    const json = await response.json();
 
     if (response.ok) {
-      dispatch({ type: 'DELETE_JOB', payload: json });
+      onDeleteJob(job._id); // Call the onDeleteJob callback to update the job list
     }
   };
 
   return (
-    <div className="job-details">
-      <h4>{job.company}</h4>
-      <p><strong>Position: </strong>{job.position}</p>
-      <p><strong>Application Status: </strong>{job.appStatus}</p>
-      <p><strong>Next Steps: </strong>{job.nextSteps}</p>
-      <p><strong>Deadline: </strong>{job.deadline}</p>
-      <p><strong>Date Applied: </strong>{job.dateApplied}</p>
-      <p><strong>Important Date: </strong>{job.importantDate}</p>
-      <p><strong>Notes: </strong>{job.notes}</p>
-      <p><strong>Final Result: </strong>{job.finalResult}</p>
-  
-      <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
-    </div>
-  );
-};
-
-export default JobDisplay; 
+        <div className="job-details">
+          <h4>{job.company}</h4>
+          <p><strong>Position: </strong>{job.position}</p>
+          <p><strong>Application Status: </strong>{job.appStatus}</p>
+          <p><strong>Next Steps: </strong>{job.nextSteps}</p>
+          <p><strong>Deadline: </strong>{job.deadline}</p>
+          <p><strong>Date Applied: </strong>{job.dateApplied}</p>
+          <p><strong>Important Date: </strong>{job.importantDate}</p>
+          <p><strong>Notes: </strong>{job.notes}</p>
+          <p><strong>Final Result: </strong>{job.finalResult}</p>
+      
+          <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
+        </div>
+      );
+    };
+    
+    export default JobDisplay; 
+    
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,41 +1,43 @@
-import { useEffect }from 'react'
-import { useJobsContext } from "../hooks/useJobsContext"
-import { useAuthContext } from "../hooks/useAuthContext"
-
-import JobDisplay from '../components/JobDisplay'
-import JobForm from '../components/JobForm'
+import React, { useEffect } from 'react';
+import { useJobsContext } from '../hooks/useJobsContext';
+import { useAuthContext } from '../hooks/useAuthContext';
+import JobDisplay from '../components/JobDisplay';
+import JobForm from '../components/JobForm';
 
 const Home = () => {
-  const {jobs, dispatch} = useJobsContext()
-  const {user} = useAuthContext()
+  const { jobs, dispatch } = useJobsContext();
+  const { user } = useAuthContext();
 
   useEffect(() => {
-    if (!user) return; 
+    if (!user) return;
 
     const fetchJobs = async () => {
       const response = await fetch('http://localhost:4000/jobs', {
-        headers: {'Authorization': `Bearer ${user.token}`},
-      })
-      const json = await response.json()
+        headers: { 'Authorization': `Bearer ${user.token}` },
+      });
+      const json = await response.json();
 
       if (response.ok) {
-        dispatch({type: 'SET_JOBS', payload: json})
+        dispatch({ type: 'SET_JOBS', payload: json });
       }
-    }
+    };
 
-    fetchJobs()
-  }, [dispatch, user])  
+    fetchJobs();
+  }, [dispatch, user]);
+
+  const handleDeleteJob = (jobId) => {
+    dispatch({ type: 'DELETE_JOB', payload: { _id: jobId } });
+  };
 
   return (
     <div className="home">
       <div className="jobs">
         {jobs && jobs.map((job) => (
-          <JobDisplay key={job._id} job={job} />
+          <JobDisplay key={job._id} job={job} onDeleteJob={handleDeleteJob} />
         ))}
       </div>
       <JobForm />
     </div>
-  )
-}
-
-export default Home
+  );
+        }
+export default Home;
