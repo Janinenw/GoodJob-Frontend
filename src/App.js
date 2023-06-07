55d22530038efe6cc4766a6cb1a41d11058dffba
Janinenw <janine.wexler@gmail.com>
2023-06-07 16:23:32 -0400

    welcome page, logout button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
+// import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+// import { useAuthContext } from './hooks/useAuthContext'
+
+// import Home from './pages/Home'
+// import Login from './pages/Login'
+// import Navbar from './components/Nav.js'
+// import Signup from './pages/Signup'
+
+
+// function App() {
+//   const { user } = useAuthContext()
+//   const BASE_URL = process.env.REACT_APP_BASE_URL; 
+//   return (
+//     <div className="App">
+//       <BrowserRouter>
+//         <Navbar />
+//         <div className="pages">
+//           <Routes>
+//             <Route 
+//               path="/" 
+//               element={user ? <Home baseUrl={BASE_URL} /> : <Navigate to="/login" />} 
+//             /> 
+//             <Route 
+//               path="/login" 
+//               element={!user ? <Login baseUrl={BASE_URL} /> : <Navigate to="/" />} 
+//             /> 
+//             <Route 
+//               path="/signup" 
+//               element={!user ? <Signup baseUrl={BASE_URL} /> : <Navigate to="/" />} 
+//             /> 
+//           </Routes>
+//         </div>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useAuthContext } from './hooks/useAuthContext'
+import Welcome from './pages/Welcome'  
 
 import Home from './pages/Home'
 import Login from './pages/Login'
@@ -18,15 +58,19 @@ function App() {
           <Routes>
             <Route 
               path="/" 
-              element={user ? <Home baseUrl={BASE_URL} /> : <Navigate to="/login" />} 
+              element={!user ? <Welcome/> : <Navigate to="/home" />} 
+            />
+            <Route 
+              path="/home" 
+              element={user ? <Home baseUrl={BASE_URL} /> : <Navigate to="/" />} 
             /> 
             <Route 
               path="/login" 
-              element={!user ? <Login baseUrl={BASE_URL} /> : <Navigate to="/" />} 
+              element={!user ? <Login baseUrl={BASE_URL} /> : <Navigate to="/home" />} 
             /> 
             <Route 
               path="/signup" 
-              element={!user ? <Signup baseUrl={BASE_URL} /> : <Navigate to="/" />} 
+              element={!user ? <Signup baseUrl={BASE_URL} /> : <Navigate to="/home" />} 
             /> 
           </Routes>
         </div>
@@ -36,4 +80,3 @@ function App() {
 }
 
 export default App;
-
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,36 +1,36 @@
-import { Link } from 'react-router-dom'
-import { useLogout } from '../hooks/useLogout'
-import { useAuthContext } from '../hooks/useAuthContext'
+import { useNavigate } from 'react-router-dom';
+import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 const Nav = () => {
-  const { logout } = useLogout()
-  const { user } = useAuthContext()
+  const navigate = useNavigate();
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
 
-  const handleClick = () => {
-    logout()
-  }
+  const handleClick = async () => {
+    await logout();
+    navigate('/');
+  };
 
   return (
     <header>
       <div className="container">
         <nav>
           {user && (
-            <div>
-              <span>{user.email}</span>
-              <button onClick={handleClick}>Log out</button>
-            </div>
-          )}
-          {!user && (
-            <div>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Signup</Link>
+            <div className="flex justify-end items-center">
+              <span className="mr-5">Go get' em, {user.name}!</span>
+              <button 
+                onClick={handleClick} 
+                className="bg-yellow-500 hover:bg-yellow-400 text-white font-bold py-2 px-4 rounded-lg"
+              >
+                Log out
+              </button>
             </div>
           )}
         </nav>
       </div>
     </header>
-  )
-}
+  );
+};
 
 export default Nav;
-
