91c9e0f0ccc59801ddc4160f2f8b594c396bb0a7
Janinenw <janine.wexler@gmail.com>
2023-06-10 11:20:30 -0400

    changed formatting of form

--- src/components/JobDisplay.js
+++ src/components/JobDisplay.js
@@ -1,66 +1,66 @@
 
-import React from 'react';
-import { useAuthContext } from '../hooks/useAuthContext';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import React from 'react';
+// import { useAuthContext } from '../hooks/useAuthContext';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
-const JobDisplay = ({ job, onDeleteJob, onEditJob }) => {
-  const { user } = useAuthContext();
+// const JobDisplay = ({ job, onDeleteJob, onEditJob }) => {
+//   const { user } = useAuthContext();
 
-  const handleClick = async () => {
-    if (!user) {
-      return;
-    }
+//   const handleClick = async () => {
+//     if (!user) {
+//       return;
+//     }
 
-    onDeleteJob(job._id);
-  };
+//     onDeleteJob(job._id);
+//   };
  
-  const handleEditClick = () => {
-    onEditJob(job);
-  };
+//   const handleEditClick = () => {
+//     onEditJob(job);
+//   };
 
-  return (
-    <div className="container">
-      <table className="table table-striped table-bordered">
-        <thead>
-          <tr>
-            <th>Company</th>
-            <th>Position</th>
-            <th>Application Status</th>
-            <th>Next Steps</th>
-            <th>Deadline</th>
-            <th>Date Applied</th>
-            <th>Important Date</th>
-            <th>Notes</th>
-            <th>Final Result</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{job.company}</td>
-            <td>{job.position}</td>
-            <td>{job.appStatus}</td>
-            <td>{job.nextSteps}</td>
-            <td>{job.deadline}</td>
-            <td>{job.dateApplied}</td>
-            <td>{job.importantDate}</td>
-            <td>{job.notes}</td>
-            <td>{job.finalResult}</td>
-            <td>
-              <button className="btn btn-danger" onClick={handleClick}>Delete</button>
-            </td>
-            <td>
-              <button className="btn btn-primary" onClick={handleEditClick}>Edit</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
-};
+//   return (
+//     <div className="container">
+//       <table className="table table-striped table-bordered">
+//         <thead>
+//           <tr>
+//             <th>Company</th>
+//             <th>Position</th>
+//             <th>Application Status</th>
+//             <th>Next Steps</th>
+//             <th>Deadline</th>
+//             <th>Date Applied</th>
+//             <th>Important Date</th>
+//             <th>Notes</th>
+//             <th>Final Result</th>
+//             <th></th>
+//             <th></th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           <tr>
+//             <td>{job.company}</td>
+//             <td>{job.position}</td>
+//             <td>{job.appStatus}</td>
+//             <td>{job.nextSteps}</td>
+//             <td>{job.deadline}</td>
+//             <td>{job.dateApplied}</td>
+//             <td>{job.importantDate}</td>
+//             <td>{job.notes}</td>
+//             <td>{job.finalResult}</td>
+//             <td>
+//               <button className="btn btn-danger" onClick={handleClick}>Delete</button>
+//             </td>
+//             <td>
+//               <button className="btn btn-primary" onClick={handleEditClick}>Edit</button>
+//             </td>
+//           </tr>
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
 
-export default JobDisplay;
+// export default JobDisplay;
 
 
 // toggle each job
@@ -182,3 +182,66 @@ export default JobDisplay;
 // export default JobDisplay;
 
 
+import React from 'react';
+import { useAuthContext } from '../hooks/useAuthContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './JobDisplay.css'; // Import the CSS file
+
+const JobDisplay = ({ job, onDeleteJob, onEditJob }) => {
+  const { user } = useAuthContext();
+
+  const handleClick = async () => {
+    if (!user) {
+      return;
+    }
+
+    onDeleteJob(job._id);
+  };
+ 
+  const handleEditClick = () => {
+    onEditJob(job);
+  };
+
+  return (
+    <div className="container">
+      <table className="table table-striped table-bordered fixed-layout">
+        <thead>
+          <tr>
+            <th className="col-width">Company</th>
+            <th className="col-width">Position</th>
+            <th className="col-width">Application Status</th>
+            <th className="col-width">Next Steps</th>
+            <th className="col-width">Deadline</th>
+            <th className="col-width">Date Applied</th>
+            <th className="col-width">Important Date</th>
+            <th className="col-width">Notes</th>
+            <th className="col-width">Final Result</th>
+            <th className="col-width"></th>
+            <th className="col-width"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{job.company}</td>
+            <td>{job.position}</td>
+            <td>{job.appStatus}</td>
+            <td>{job.nextSteps}</td>
+            <td>{job.deadline}</td>
+            <td>{job.dateApplied}</td>
+            <td>{job.importantDate}</td>
+            <td>{job.notes}</td>
+            <td>{job.finalResult}</td>
+            <td>
+              <button className="btn btn-danger" onClick={handleClick}>Delete</button>
+            </td>
+            <td>
+              <button className="btn btn-primary" onClick={handleEditClick}>Edit</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default JobDisplay;
