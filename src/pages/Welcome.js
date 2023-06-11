4ac254d1a55fd6a7c3ed7b539732d94ee84a7b32
Janinenw <janine.wexler@gmail.com>
2023-06-11 15:37:25 -0400

    more buttons

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -6,7 +6,7 @@ const Welcome = () => {
     return (
         <div className="min-h-screen bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 flex flex-col items-center justify-center">
             <div className="container mx-auto text-center p-5 bg-white rounded-lg shadow-2xl">
-                <h1 className="text-6xl font-bold text-purple-600 mb-5 border-b-2 pb-2">Welcome to Good Job!</h1>
+                <h1 className="text-6xl font-bold text-purple-600 mb-5 border-b-2 pb-2">Welcome to GoodJob!</h1>
                 <p className="text-2xl text-blue-600 mb-10">You're probably not excited, but we sure are!</p>
                 <div className="flex justify-center space-x-5 mb-5">
                     <Link to="/login">
